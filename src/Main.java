49e8ff1dd6204f0d44ab22ecc5f999e17b07f54b
ahmadim11 <madina.ahmadi01@gmail.com>
2022-12-18 10:22:32 -0500

    The basic structure of the solary system

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("The Basic Structure is ready pealse write down your code in this project");
+    }
+}
